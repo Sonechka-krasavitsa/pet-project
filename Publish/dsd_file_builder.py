95a314b5986cf7ff9189a8dfa373e0ccaa95e918
Sonechka-krasavitsa <soalekseeva@mail.ru>
2020-01-20 17:53:24 +0300

    отредактирована функция для поиска маркеров

--- Publish/dsd_file_builder.py
+++ Publish/dsd_file_builder.py
@@ -15,16 +15,21 @@ dsd_temporary_file = appdir / "tmp.dsd"
 
 def get_marker(text):
     '''
-    регулярное выражение, которое ищет маркеры вида ${xxx_xxx}  в тексте
+    регулярное выражение, которое ищет маркеры вида ${xxx_xxx} в тексте
     группа marker возвращает имя параметра xxx_xxx
     '''
-    # reg_exp = r'\${(?P<marker>\w{1,})}'
-    reg_exp = r'\${\w{1,}}'
-    return re.findall(reg_exp, text)
+    reg_exp = r'(?P<fullmatch>\${(?P<marker>\w{1,})})'
+    match_list = re.findall(reg_exp, text)
+    fullmatch_list = [match_list[i][0] for i in range(len(match_list))]
+    marker_list = [match_list[j][1] for j in range(len(match_list))]
+    return fullmatch_list, marker_list
 
 
 dsd_temporary_file.touch()
 # dsd_temporary_file.write_text("[DWF6Version]\nVer=1\n[DWF6MinorVersion]\nMinorVer=1\n")
 # dsd_temporary_file.write_text(dsd_temporary_file.read_text().replace('DWG', 'huy'))
-my_lovely_list = get_marker(sheet_info_block.read_text())
-print(my_lovely_list)
+fullmatch_list = get_marker(sheet_info_block.read_text())[0]
+marker_list = get_marker(sheet_info_block.read_text())[1]
+print(fullmatch_list)
+print(marker_list)
+
