00bf46c3fff0b4968661c1f5b95d303a1587cd40
Sonechka-krasavitsa <soalekseeva@mail.ru>
2020-01-21 09:21:01 +0300

    копируем текст с исходного файла и кидаем в новый

--- Publish/dsd_file_builder.py
+++ Publish/dsd_file_builder.py
@@ -17,11 +17,8 @@ def get_marker(text):
     регулярное выражение, которое ищет маркеры вида ${xxx_xxx} в тексте
     группа marker возвращает имя параметра xxx_xxx
     '''
-    reg_exp = r'(\${(\w{1,})})'
-    match_list = re.findall(reg_exp, text)
-    fullmatch_list = [match_list[i][0] for i in range(len(match_list))]
-    marker_list = [match_list[j][1] for j in range(len(match_list))]
-    return fullmatch_list, marker_list
+    reg_exp = r'\${\w{1,}}'
+    return re.findall(reg_exp, text)
 
 
 def sheet_dict(layout_name, file_name, dwg_file_path):
@@ -35,14 +32,14 @@ def sheet_dict(layout_name, file_name, dwg_file_path):
 dsd_temporary_file.touch()
 # dsd_temporary_file.write_text("[DWF6Version]\nVer=1\n[DWF6MinorVersion]\nMinorVer=1\n")
 # dsd_temporary_file.write_text(dsd_temporary_file.read_text().replace('DWG', 'huy'))
-fullmatch_list = get_marker(sheet_info_block.read_text())[0]
-marker_list = get_marker(sheet_info_block.read_text())[1]
-print(fullmatch_list)
+marker_list = get_marker(sheet_info_block.read_text())
 print(marker_list)
 layout_name = 'ЩР-СС.ОФ2.L3.1~1'
 sheet_dict = sheet_dict(layout_name, file_name, dwg_file_path)
 print(sheet_dict)
 print(len(sheet_dict))
-for i in range(len(fullmatch_list)):
-    sheet_info_block.write_text(
-        sheet_info_block.read_text().replace(fullmatch_list[i], sheet_dict[fullmatch_list[i]]))
+tmp_text = sheet_info_block.read_text()
+print(tmp_text)
+for i in range(len(marker_list)):
+    tmp_text = tmp_text.replace(marker_list[i], sheet_dict[marker_list[i]])
+print(tmp_text)
