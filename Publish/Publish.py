5e6dcc58282921afe8109e0228babe4a3a26b9ce
Sonechka-krasavitsa <soalekseeva@mail.ru>
2020-01-23 18:18:42 +0300

    паблиш работает

--- Publish/Publish.py
+++ Publish/Publish.py
@@ -11,7 +11,7 @@ dsd_temporary_file = appdir / "tmp.dsd"
 
 def get_marker(text):
     '''
-    регулярное выражение, которое ищет маркеры вида ${xxx_xxx} в тексте шоблонов
+    регулярное выражение, которое ищет маркеры вида ${xxx_xxx} в тексте шоблонов1
     '''
     reg_exp = r'\${\w{1,}}'
     return re.findall(reg_exp, text)
@@ -37,7 +37,7 @@ def setting_dict(pdf_target_folder, file_name, dsd_temporary_file):
     setting_dict = {}
     setting_dict['${pdf_file_path}'] = pdf_target_folder + '\\' + re.sub(r'\.dwg', r'.pdf', file_name)
     setting_dict['${target_folder}'] = pdf_target_folder
-    setting_dict['${dsd_file_path}'] = dsd_temporary_file.as_posix().replace('/', '\\')
+    setting_dict['${dsd_file_path}'] = str(dsd_temporary_file)
     return setting_dict
 
 
@@ -48,52 +48,52 @@ def path_correction(path_iter):
     return new_path
 
 
-'''Запрашиваем у пользователя файлы для паблиша и папку, куда сохраняем PDF'''
-tmp_file_list = filedialog.askopenfilenames()
-tmp_pdf_target_folder = filedialog.askdirectory()
+if __name__ == '__main__':
 
-'''Меняем / на \\'''
-file_list = path_correction(tmp_file_list)
-pdf_target_folder = tmp_pdf_target_folder.replace('/', '\\')
+    '''Запрашиваем у пользователя файлы для паблиша и папку, куда сохраняем PDF'''
+    tmp_file_list = filedialog.askopenfilenames()
+    tmp_pdf_target_folder = filedialog.askdirectory()
 
-app = win32.gencache.EnsureDispatch("AutoCAD.Application")
-for i in file_list:
-    '''Открываем файл, берем информацию о листах и имя файла'''
-    doc = app.Documents.Open(i, False)
-    file_name = doc.Name
-    all_layout_info = doc.Layouts
-    layout_list = []
-    for j in all_layout_info:
-        if j.Name == "Model":
-            continue
-        else:
-            layout_list.append(j.Name)
-    # all_layout_info.remove()
+    '''Меняем / на \\'''
+    file_list = path_correction(tmp_file_list)
+    pdf_target_folder = tmp_pdf_target_folder.replace('/', '\\')
 
-    '''Генерируем файл dsd'''
-    '''Собираем блок с информацией по листам'''
-    main_text = "[DWF6Version]\nVer=1\n[DWF6MinorVersion]\nMinorVer=1"
-    marker_list = get_marker(sheet_info_block.read_text())
-    for j in range(len(layout_list)):
-        sheet_info_dict = sheet_dict(layout_list[j], file_name, i)
-        tmp_text = sheet_info_block.read_text()
-        for k in range(len(marker_list)):
-            tmp_text = tmp_text.replace(marker_list[k], sheet_info_dict[marker_list[k]])
+    app = win32.gencache.EnsureDispatch("AutoCAD.Application")
+    for i in file_list:
+        '''Открываем файл, берем информацию о листах и имя файла'''
+        doc = app.Documents.Open(i, False)
+        file_name = doc.Name
+        all_layout_info = doc.Layouts
+        layout_list = []
+        for j in all_layout_info:
+            if j.Name == "Model":
+                continue
+            else:
+                layout_list.append(j.Name)
+
+        '''Генерируем файл dsd'''
+        '''Собираем блок с информацией по листам'''
+        main_text = "[DWF6Version]\nVer=1\n[DWF6MinorVersion]\nMinorVer=1"
+        marker_list = get_marker(sheet_info_block.read_text())
+        for j in range(len(layout_list)):
+            sheet_info_dict = sheet_dict(layout_list[j], file_name, i)
+            tmp_text = sheet_info_block.read_text()
+            for k in range(len(marker_list)):
+                tmp_text = tmp_text.replace(marker_list[k], sheet_info_dict[marker_list[k]])
+            main_text = main_text + '\n' + tmp_text
+
+        '''Собираем блок с информацией по печати'''
+        setting_info_dict = setting_dict(pdf_target_folder, file_name, dsd_temporary_file)
+        tmp_text = dsd_template_file.read_text()
+        marker_list = get_marker(tmp_text)
+        for j in range(len(marker_list)):
+            tmp_text = tmp_text.replace(marker_list[j], setting_info_dict[marker_list[j]])
         main_text = main_text + '\n' + tmp_text
+        dsd_temporary_file.touch()
+        dsd_temporary_file.write_text(main_text)
 
-    '''Собираем блок с информацией по печати'''
-    setting_info_dict = setting_dict(pdf_target_folder, file_name, dsd_temporary_file)
-    tmp_text = dsd_template_file.read_text()
-    marker_list = get_marker(tmp_text)
-    for j in range(len(marker_list)):
-        tmp_text = tmp_text.replace(marker_list[j], setting_info_dict[marker_list[j]])
-    main_text = main_text + '\n' + tmp_text
-    dsd_temporary_file.touch()
-    dsd_temporary_file.write_text(main_text)
-
-    '''Посылаем на печать файл'''
-    doc.SetVariable('FILEDIA', 0)
-    doc.SendCommand("-PUBLISH\n" + dsd_temporary_file.as_posix().replace('/', '\\') + '\n')
-    # doc.SendCommand(dsd_temporary_file.as_posix()+' \n')
-    doc.SetVariable('FILEDIA', 1)
-    doc.Close(False)
+        '''Посылаем на печать файл'''
+        doc.SetVariable('FILEDIA', 0)
+        doc.SendCommand("-PUBLISH\n" + str(dsd_temporary_file) + '\n')
+        doc.SetVariable('FILEDIA', 1)
+        doc.Close(False)
